b4d1829da555e46e7819aabe7c7ce359e89c8c1c
Tobin Cooney <tobin.cooney@gmail.com>
2022-06-05 18:31:21 -0400

    new updates from site-3 (style, build directory, symlinks, etc.)

--- config.sh
+++ config.sh
@@ -1,4 +1,6 @@
-motd=`cat motd`
+#!/bin/bash
+
+#motd=`cat NOTES`
 
 
 # define the filler html variables
@@ -22,10 +24,14 @@ function head_to_toc_build {
 			</tr>
 			<tr>
 				<td valign='top' class='menu'>
+					<h2>site title</h2><br>
 					<details open>
 						<summary><b>contents</b></summary>
 						<ul class='menu'>
 							<li><a href='index.html'>home</a></li>
+
+							<!-- begin table of contents dump -->
+
 	"
 	echo "$head_to_toc"
 }
@@ -36,9 +42,15 @@ toc_to_main="
 					<code>you can also put a footer thing here</code>
 				</td>
 				<td valign='top' class='main'>
+
+					<!-- begin main content dump -->
+
 "
 
 main_to_end="
+
+					<!-- end main content dump -->
+
 				</td>
 			</tr>
 		</table>
@@ -48,10 +60,10 @@ main_to_end="
 
 # convert md file input to html string
 function htmlify {
-	yield=`cat $1 | sed 's/^/<p>/g' | sed 's#$#</p>#g'`
+	yield=`cat source/$1 | sed 's/^/<p>/g' | sed 's#$#</p>#g'`
 	yield=`echo "$yield" | \
+		sed '/<p></s/<\/p>//' | \
 		sed 's#<p><#<#' | \
-		sed 's#></p>$#>#' | \
 		sed '/<p># /s/$/ ::<\/h1><\/center>/' | \
 		sed '/<p># /s/<\/p>//g' | \
 		sed 's/<p># /<center><h1>:: /' | \
@@ -68,40 +80,68 @@ echo "motd: $motd"
 
 # build toc
 echo "building table of contents..."
-for f in $(ls -r *.md | sed '/000/d')
+for f in $(ls -r source/*.md | sed '/000/d')
 do
-	entry_num=`echo $f | cut -c 1-3`
-	title=`echo $f | sed 's/.md//g' | sed 's/^....//' | sed 's/-/ /g'`
-	html_name="`echo $f | sed 's/.md//g'`.html"
+	file=`echo $f | sed 's/source\///g'`
+	entry_num=`echo $file | cut -c 1-3`
+	title=`echo $file | sed 's/.md//g' | sed 's/^....//' | sed 's/-/ /g'`
+	html_name="`echo $file | sed 's/.md//g'`.html"
 	toc+="<li><a href='$html_name'>$entry_num: $title</a></li>"
-	echo "[$entry_num: $title]($html_name)"
+	echo ". [$entry_num: $title]($html_name)"
 done
 
 
-# assemble pages
-for f in $(ls -r *.md | sed '/000/d')
-do
-	entry_num=`echo $f | cut -c 1-3`
-	title=`echo $f | sed 's/.md//g' | sed 's/^....//' | sed 's/-/ /g'`
-	head_to_toc=$( head_to_toc_build "$title" )
-	echo "htmlifying $f..."
-	main=$( htmlify $f )
+echo -n "trying to generate site/ directory... "
+mkdir site
+if test $? -eq 0
+then
+	echo "ok"
+fi
 
-	page="$head_to_toc$toc$toc_to_main$main$main_to_end"
-	html_name="`echo $f | sed 's/.md//g'`.html"
+# assemble pages
+for f in $(ls -r source/*.md | sed '/000/d')
+do
+	file=`echo $f | sed 's/source\///g'`
+	entry_num=`echo $file | cut -c 1-3`
+	title=`echo $file | sed 's/.md//g' | sed 's/^....//' | sed 's/-/ /g'`
+	head_to_toc=$( head_to_toc_build "$entry_num $title" )
+	echo -n "htmlifying $f... "
+	main=$( htmlify $file )
+
+	page="$head_to_toc$toc$toc_to_main<center><code>$entry_num</code></center>$main$main_to_end"
+	html_name="`echo $file | sed 's/.md//g'`.html"
 
 	echo "writing $html_name..."
-	echo "$page" > $html_name
+	echo "$page" > site/$html_name
 done
 
 function build_index {
-	src=`ls 000*.md`
-	echo "building index from $src..."
-	head_to_toc=$( head_to_toc_build home )
-	main=$( htmlify $src )
+	src=`ls source/000*.md`
+	file=`echo $src | sed 's/source\///g'`
+	echo "building index.html from $src..."
+	head_to_toc=$( head_to_toc_build catalog )
+	main=$( htmlify $file )
 	page="$head_to_toc$toc$toc_to_main$main$main_to_end"
-	html_name="`echo $src | sed 's/.md//g'`.html"
-	echo "$page" > index.html
+	html_name="`echo $file | sed 's/.md//g'`.html"
+	echo "$page" > site/index.html
 }
 
 build_index
+
+echo -n "creating symlink of source/style.css at site/style.css... "
+cd site
+ln -sf ../source/style.css style.css
+cd ..
+echo ""
+
+echo -n "creating symlink of source/icon.ico at site/icon.ico... "
+cd site
+ln -sf ../source/icon.ico icon.ico
+cd ..
+echo ""
+
+echo -n "creating symlink of source/media/ at site/media... "
+cd site
+ln -sf ../source/media media
+cd ..
+echo ""
