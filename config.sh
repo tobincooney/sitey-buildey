a90e79efb5566a1e378f3873dfe5d468fae0e596
Tobin Cooney <tobin.cooney@gmail.com>
2022-07-04 00:57:50 -0400

    bring in changes from site-3 0b5f162

--- config.sh
+++ config.sh
@@ -2,16 +2,13 @@
 
 #motd=`cat NOTES`
 
-
-# define the filler html variables
-# $toc and $main are defined in the loop
+date=`date`
 
 function head_to_toc_build {
-	head_to_toc="
-<!DOCTYPE html>
+	head_to_toc="<!DOCTYPE html>
 <html>
 	<head>
-		<link rel='icon' type='image/png' href='icon.ico' />
+		<link rel='icon' type='image/png' href='../media/icon.ico' />
 		<link rel='stylesheet' type='text/css' href='style.css' />
 		<title>$1</title>
 	</head>
@@ -24,22 +21,29 @@ function head_to_toc_build {
 			</tr>
 			<tr>
 				<td valign='top' class='menu'>
-					<h2>site title</h2><br>
+					<h2><a style='text-decoration:none; color:black;' href='home.html'>site title</a></h2><br>
 					<details open>
 						<summary><b>contents</b></summary>
 						<ul class='menu'>
-							<li><a href='index.html'>home</a></li>
+							<li><a href='home.html'>Home</a></li>
+							<li><a href='updates.html'>Updates</a></li>
+							<li><a href='index.html'>Index</a></li>
+						</ul>
+						<h3>Auto-generated</h3>
+						<ul class='menu'>
 
 							<!-- begin table of contents dump -->
 
-	"
+"
 	echo "$head_to_toc"
 }
 
 toc_to_main="
+
+							<!-- end table of contents dump -->
+
 						</ul>
 					</details><br>
-					<code>you can also put a footer thing here</code>
 				</td>
 				<td valign='top' class='main'>
 
@@ -51,6 +55,13 @@ main_to_end="
 
 					<!-- end main content dump -->
 
+				</td>
+			</tr>
+			<tr><td></td>
+				<td>
+					<br><hr><br>
+					<p>Last updated $date</p>
+					<p>[[&copy; Copyright goes here]]</p>
 				</td>
 			</tr>
 		</table>
@@ -75,73 +86,189 @@ function htmlify {
 	echo "$yield"
 }
 
+function backlink {
+	l="0"
+	for b in $( ls -tr source/*.md source/*.html )
+	do
+		pretty=`head -1 $b | sed 's/# //' | sed 's/<center><h1>:: //' | sed 's/ ::<\/h1><\/center>//'`
+		cat $b | grep "$id" | grep -q "<a href="
+		if test $? -eq 0
+		then
+			l=`echo "$l + 1" | bc`
+			echo "<li><a href='$b'>$pretty</a></li>" | sed 's/.md/.html/' | sed 's/source\///'
+		fi
+	done
+	for m in $( ls -tr source/updates/*.html )
+	do
+		cat $m | grep "$id" | grep -q "<a href="
+		if test $? -eq 0
+		then
+			echo "<li><a href='updates.html'>Updates</a></li>"
+			l=`echo "$l + 1" | bc`
+		fi
+	done
+}
+
 echo "configuring..."
-echo "motd: $motd"
 
-# build toc
-echo "building table of contents..."
-for f in $(ls -r source/*.md | sed '/000/d')
+echo "building index..."
+for f in $( ls source/*.md source/*.html )
 do
-	file=`echo $f | sed 's/source\///g'`
-	entry_num=`echo $file | cut -c 1-3`
-	title=`echo $file | sed 's/.md//g' | sed 's/^....//' | sed 's/-/ /g'`
-	html_name="`echo $file | sed 's/.md//g'`.html"
-	toc+="<li><a href='$html_name'>$entry_num: $title</a></li>"
-	echo ". [$entry_num: $title]($html_name)"
+	html_name="`echo $f | sed 's/source\///g' | sed 's/.md//g' | sed 's/.html//g'`.html"
+	pretty=`head -1 $f | sed 's/# //' | sed 's/<center><h1>:: //' | sed 's/ ::<\/h1><\/center>//'`
+	index+="<li><a href='$html_name'>$pretty</a></li>"
+	# take this next line out if you make your own static table of contents above
+	toc+="<li><a href='$html_name'>$pretty</a></li>"
+	echo ". [$pretty]($html_name)"
 done
-
 
 echo -n "trying to generate site/ directory... "
 mkdir site
 if test $? -eq 0
 then
 	echo "ok"
+else
+	echo -n "removing existing pages... "
+	rm site/*.html
+	rm site/rss.xml
+	echo "ok"
 fi
 
-# assemble pages
-for f in $(ls -r source/*.md | sed '/000/d')
+
+echo "running htmlification of source/*.md..."
+
+for f in $(ls -tr source/*.md | sed '/updates.md/d')
 do
 	file=`echo $f | sed 's/source\///g'`
-	entry_num=`echo $file | cut -c 1-3`
-	title=`echo $file | sed 's/.md//g' | sed 's/^....//' | sed 's/-/ /g'`
-	head_to_toc=$( head_to_toc_build "$entry_num $title" )
-	echo -n "htmlifying $f... "
+	pretty=`head -1 $f | sed 's/# //' | sed 's/<center><h1>:: //' | sed 's/ ::<\/h1><\/center>//'`
+	head_to_toc=$( head_to_toc_build "$pretty" )
+	echo -n ". htmlifying $f... "
 	main=$( htmlify $file )
 
-	page="$head_to_toc$toc$toc_to_main<center><code>$entry_num</code></center>$main$main_to_end"
-	html_name="`echo $file | sed 's/.md//g'`.html"
+	echo -n "finding backlinks... "
+	id="`echo $file | sed 's/.md//g'`.html"
+	backlinks=$( backlink )
+	if [ -z "$backlinks" ]
+	then
+		backlink_header=""
+	else
+		backlink_header="<br><br><br><h3>Backlinks:</h3>"
+	fi
+	backlink > /dev/null
+	echo -n "$l. "
 
-	echo "writing $html_name..."
-	echo "$page" > site/$html_name
+	page="$head_to_toc$toc$toc_to_main$main$backlink_header<ul>$backlinks</ul>$main_to_end"
+
+	echo "writing $id..."
+	echo "$page" > site/$id
+done
+echo "compiling source/*.html..."
+for f in $(ls -tr source/*.html)
+do
+	file=`echo $f | sed 's/source\///g'`
+	pretty=`head -1 $f | sed 's/# //' | sed 's/<center><h1>:: //' | sed 's/ ::<\/h1><\/center>//'`
+	head_to_toc=$( head_to_toc_build "$pretty" )
+	echo -n ". reading $f... "
+	main=$( cat $f )
+
+	echo -n "finding backlinks... "
+	id=$file
+	backlinks=$( backlink )
+	if [ -z "$backlinks" ]
+	then
+		backlink_header=""
+	else
+		backlink_header="<br><br><br><h3>Backlinks:</h3>"
+	fi
+	backlink > /dev/null
+	echo -n "$l. "
+
+	page="$head_to_toc$toc$toc_to_main$main$backlink_header<ul>$backlinks</ul>$main_to_end"
+
+	echo "writing $file..."
+	echo "$page" > site/$file
 done
 
-function build_index {
-	src=`ls source/000*.md`
-	file=`echo $src | sed 's/source\///g'`
-	echo "building index.html from $src..."
-	head_to_toc=$( head_to_toc_build catalog )
-	main=$( htmlify $file )
-	page="$head_to_toc$toc$toc_to_main$main$main_to_end"
-	html_name="`echo $file | sed 's/.md//g'`.html"
-	echo "$page" > site/index.html
-}
 
-build_index
+# create index page
+echo -n "creating index page... "
+head_to_toc=$( head_to_toc_build "Index" )
+main="<center><h1>:: Index ::</h1></center>
+<strong>this is an automatically-generated index of the site's source files</strong>
+<ul>
+$index
+</ul>"
+echo -n "finding backlinks... "
+id="index.html"
+backlinks=$( backlink )
+if [ -z "$backlinks" ]
+then
+	backlink_header=""
+else
+	backlink_header="<br><br><br><h3>Backlinks:</h3>"
+fi
+backlink > /dev/null
+echo "$l. "
+page="$head_to_toc$toc$toc_to_main$main$backlink_header<ul>$backlinks</ul>$main_to_end"
+echo "$page" > site/index.html
+
+echo "generating rss feed and compiling updates.html out of source/*.html..."
+head_to_toc=$( head_to_toc_build "Updates" )
+main="<center><h1>:: Updates ::</h1></center>
+<strong>View the <a href='rss.xml'>RSS feed</a> built from this same content</strong>"
+
+for f in $(ls -t source/updates/*.html)
+do
+	echo ". adapting $f..."
+	item_title=`head -1 $f | sed 's/<center><h2>- /  <title>/' | sed 's/ -<\/h2><\/center>/<\/title>/'`
+	item_date=`head -2 $f | tail -1 | sed 's/<center><code>/  <pubDate>/' | sed 's/<\/code><\/center>/<\/pubDate>/'`
+	item_main=`tail -n +3 $f`
+	item="<item>
+$item_title
+  <link>https://example.com</link>
+$item_date
+  <description>
+<![CDATA[$item_main]]>
+  </description>
+</item>
+"
+	itemstack+="$item"
+	main+=`cat $f`
+done
+
+rss="<?xml version='1.0' encoding='UTF-8' ?>
+<rss version='2.0'>
+<channel>
+<title>Your Site Title</title>
+<link>https://example.org</link>
+<description>Description words woo</description>
+<lastBuildDate>$date</lastBuildDate>
+<image>
+  <url>https://example-image.jpg</url>
+  <title>more words</title>
+  <link>i don't actually know what this field is for</link>
+</image>
+$itemstack</channel></rss>"
+echo ". writing rss.xml..."
+echo "$rss" > site/rss.xml
+
+echo -n ". writing updates.html... "
+echo -n "finding backlinks... "
+id="updates.html"
+backlinks=$( backlink )
+if [ -z "$backlinks" ]
+then
+	backlink_header=""
+else
+	backlink_header="<br><br><br><h3>Backlinks:</h3>"
+fi
+backlink > /dev/null
+echo "$l. "
+page="$head_to_toc$toc$toc_to_main$main$backlink_header<ul>$backlinks</ul>$main_to_end"
+echo "$page" > site/updates.html
 
 echo -n "creating symlink of source/style.css at site/style.css... "
 cd site
 ln -sf ../source/style.css style.css
-cd ..
-echo ""
-
-echo -n "creating symlink of source/icon.ico at site/icon.ico... "
-cd site
-ln -sf ../source/icon.ico icon.ico
-cd ..
-echo ""
-
-echo -n "creating symlink of source/media/ at site/media... "
-cd site
-ln -sf ../source/media media
 cd ..
 echo ""
---
+++ source/example_md.md
@@ -0,0 +1,5 @@
+# Example Page (Markdown)
+
+Here's an example markdown page!
+You can write words on it...
+See also the <a href="example_html.html">html example</a> page.
